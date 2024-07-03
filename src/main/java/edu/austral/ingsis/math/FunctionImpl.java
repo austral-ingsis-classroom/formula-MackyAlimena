4aefcaff773a69c3afe10295c31ac43123fc9d5d
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-07-03 10:22:05 -0300

    first idea for Function

---
+++ src/main/java/edu/austral/ingsis/math/FunctionImpl.java
@@ -0,0 +1,10 @@
+package edu.austral.ingsis.math;
+
+import java.util.Map;
+
+public abstract class FunctionImpl implements Function {
+    @Override
+    public String print() {
+        return this.getClass().getSimpleName();
+    }
+}
