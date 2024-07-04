98c99c752e5cc99b57b2598b5f3aa8c86f665693
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-07-03 23:44:36 -0300

    resolution tests passing for ResolutionTest

--- src/test/java/edu/austral/ingsis/math/ResolutionTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionTest.java
@@ -5,61 +5,63 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class ResolutionTest {
 
   /** Case 1 + 6 */
   @Test
   public void shouldResolveSimpleFunction1() {
-    final Double result = MathEvaluator.evaluate("1 + 6");
+    final Double result = MathEvaluator.evaluate("1 + 6", new HashMap<>());
     assertThat(result, equalTo(7d));
   }
 
   /** Case 12 / 2 */
   @Test
   public void shouldResolveSimpleFunction2() {
-    final Double result = MathEvaluator.evaluate("12 / 2");
+    final Double result = MathEvaluator.evaluate("12 / 2", new HashMap<>());
     assertThat(result, equalTo(6d));
   }
 
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldResolveSimpleFunction3() {
-    final Double result = MathEvaluator.evaluate("(9 / 2) * 3");
+    final Double result = MathEvaluator.evaluate("(9 / 2) * 3", new HashMap<>());
     assertThat(result, equalTo(13.5d));
   }
 
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldResolveSimpleFunction4() {
-    final Double result = MathEvaluator.evaluate("(27 / 6) ^ 2");
+    final Double result = MathEvaluator.evaluate("(27 / 6) ^ 2", new HashMap<>());
     assertThat(result, equalTo(20.25d));
   }
 
   /** Case 36 ^ (1/2) */
   @Test
   public void shouldResolveSimpleFunction5() {
-    final Double result = MathEvaluator.evaluate("36 ^ (1/2)");
+    final Double result = MathEvaluator.evaluate("36 ^ (1/2)", new HashMap<>());
     assertThat(result, equalTo(6d));
   }
 
   /** Case |136| */
   @Test
   public void shouldResolveSimpleFunction6() {
-    final Double result = MathEvaluator.evaluate("|136|");
+    final Double result = MathEvaluator.evaluate("|136|", new HashMap<>());
     assertThat(result, equalTo(136d));
   }
 
   /** Case |-136| */
   @Test
   public void shouldResolveSimpleFunction7() {
-    final Double result = MathEvaluator.evaluate("|-136|");
+    final Double result = MathEvaluator.evaluate("|-136|", new HashMap<>());
     assertThat(result, equalTo(136d));
   }
 
   /** Case (5 - 5) * 8 */
   @Test
   public void shouldResolveSimpleFunction8() {
-    final Double result = MathEvaluator.evaluate("(5 - 5) * 8");
+    final Double result = MathEvaluator.evaluate("(5 - 5) * 8", new HashMap<>());
     assertThat(result, equalTo(0d));
   }
 }
